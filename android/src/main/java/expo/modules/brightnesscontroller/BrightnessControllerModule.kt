5423491b9f007f33fb0e7b8682116345e7f2197a
antonkolomiiets <rykerography@gmail.com>
2024-07-12 16:22:17 +0300

    android

--- android/src/main/java/expo/modules/brightnesscontroller/BrightnessControllerModule.kt
+++ android/src/main/java/expo/modules/brightnesscontroller/BrightnessControllerModule.kt
@@ -1,15 +1,44 @@
 package expo.modules.brightnesscontroller
 
-import android.content.Context
+// import android.content.Context
 import android.view.WindowManager
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+// class BrightnessControllerModule : Module() {
+//     override fun definition() = ModuleDefinition {
+//         Name("BrightnessController")
+
+//         Function("setBrightness") { brightness: Float ->
+//             val activity = appContext.currentActivity
+//             if (activity != null) {
+//                 val window = activity.window
+//                 val layoutParams = window.attributes
+//                 layoutParams.screenBrightness = brightness
+//                 window.attributes = layoutParams
+//             }
+//         }
+//     }
+// }
 
 class BrightnessControllerModule : Module() {
     override fun definition() = ModuleDefinition {
         Name("BrightnessController")
 
-        Function("setBrightness") { brightness: Float ->
+        AsyncFunction("setBrightness") { brightness: Float ->
+            GlobalScope.launch {
+                setScreenBrightness(brightness)
+            }
+        }
+    }
+
+    private suspend fun setScreenBrightness(brightness: Float) {
+        withContext(Dispatchers.Main) {
             val activity = appContext.currentActivity
             if (activity != null) {
                 val window = activity.window
@@ -20,3 +49,6 @@ class BrightnessControllerModule : Module() {
         }
     }
 }
+
+
+
